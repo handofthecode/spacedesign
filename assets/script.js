16b52ba56448346a12de822fb1428ab7854c6bef
Tovi Newman <deartovi@gmail.com>
2017-08-01 14:00:28 -0700

    JS: refactor/replace sswitch logic. HTML: add target-modal link property and remove unecessary class/id

--- assets/script.js
+++ assets/script.js
@@ -8,20 +8,8 @@ $(function() {
       e.preventDefault();
       $('#tint').fadeIn(400);
 
-      switch(true) {
-        case $link.hasClass('kevin'):
-          $('.modal.kevin').slideDown().css({top: scrollTop + 40});
-          break;
-        case $link.hasClass('louis'):
-          $('.modal.louis').slideDown().css({top: scrollTop + 40});
-          break;
-        case $link.hasClass('kasper'):
-          $('.modal.kasper').slideDown().css({top: scrollTop + 40});
-          break;
-        case $link.hasClass('chris'):
-          $('.modal.chris').slideDown().css({top: scrollTop + 40});
-          break;
-      }
+      var $modal = $('#' + $link.attr('target-modal'));
+      $modal.slideDown().css({top: scrollTop + 40});
 
     } else if ($link.hasClass('close') || $(e.target).is($('#tint'))) {
       e.preventDefault();
